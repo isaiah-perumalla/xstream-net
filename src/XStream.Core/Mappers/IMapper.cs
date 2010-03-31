ee771f98a941d10e43fb101269f5ed614ad76ed7
unknown <isaiah@.(none)>
2010-03-31 18:16:58 +0100

    integrated mapper into unmarshaller

--- src/XStream.Core/Mappers/IMapper.cs
+++ src/XStream.Core/Mappers/IMapper.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Xstream.Core.Mappers
 {
     internal interface IMapper
     {
-        void ProcessFieldsIn(Type type, IFieldProcessor fieldProcessor);
+        
+        IEnumerable<Field> GetSerializableFieldsIn(Type type);
     }
 
-    internal interface IFieldProcessor {
-        void ProcessField(FieldInfo field, string serializeFieldName);
+    internal class Field {
+        private string serializedName;
+        private FieldInfo fieldInfo;
+
+        public Field(FieldInfo field, string serializeFieldName) {
+            this.serializedName = serializeFieldName;
+            this.fieldInfo = field;
+        }
+
+        public string SerializedName {
+            get {
+                return serializedName;
+            }
+        }
+
+        public Type FieldType
+        {
+            get { return fieldInfo.FieldType; }
+        }
+
+        public void SetValue(object result, object value) {
+            fieldInfo.SetValue(result, value);
+            
+        }
     }
+
+   
 }
--- src/XStream.Core/Unmarshaller.cs
+++ src/XStream.Core/Unmarshaller.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Xml.Serialization;
@@ -7,20 +8,25 @@ using Xstream.Core.Converters;
 using Xstream.Core.Mappers;
 using xstream.Utilities;
 
-namespace Xstream.Core {
-    internal class Unmarshaller : IFieldProcessor {
+namespace Xstream.Core
+{
+    internal class Unmarshaller
+    {
         private readonly XStreamReader reader;
         private readonly UnmarshallingContext context;
         private readonly ConverterLookup converterLookup;
         private readonly IMapper mapper = new DefaultMapper();
 
-        public Unmarshaller(XStreamReader reader, UnmarshallingContext context, ConverterLookup converterLookup) {
+
+        public Unmarshaller(XStreamReader reader, UnmarshallingContext context, ConverterLookup converterLookup)
+        {
             this.reader = reader;
             this.context = context;
             this.converterLookup = converterLookup;
         }
 
-        internal object Unmarshal(Type type) {
+        internal object Unmarshal(Type type)
+        {
             var result = context.Find();
             if (result != null) return result;
             if (reader.GetAttribute(Attributes.Null) == true.ToString())
@@ -31,30 +37,26 @@ namespace Xstream.Core {
             return result;
         }
 
-        private void UnmarshalAs(object result, Type type) {
-            if (type.Equals(typeof (object))) return;
-//            this.mapper.ProcessFieldsIn(type, this);
-            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
-            foreach (var field in fields)
-            {
-                var serializeFieldName = field.Name;
-                if (field.GetCustomAttributes(typeof (DontSerialiseAttribute), true).Length != 0) continue;
-                if (field.GetCustomAttributes(typeof (XmlIgnoreAttribute), true).Length != 0) continue;
-                if (typeof (MulticastDelegate).IsAssignableFrom(field.FieldType)) continue;
-                Match match = Constants.AutoPropertyNamePattern.Match(field.Name);
-                if (match.Success)
-                    serializeFieldName = match.Result("$1");
+        private void UnmarshalAs(object result, Type type)
+        {
 
-                reader.MoveDown(serializeFieldName);
-            field.SetValue(result, ConvertField(field.FieldType));
+            if (type.Equals(typeof(object))) return;
+
+
+            foreach (var field in mapper.GetSerializableFieldsIn(type))
+            {
+                reader.MoveDown(field.SerializedName);
+                field.SetValue(result, ConvertField(field.FieldType));
                 reader.MoveUp();
-               
             }
 
             UnmarshalAs(result, type.BaseType);
         }
 
-        private object ConvertField(Type fieldType) {
+
+
+        private object ConvertField(Type fieldType)
+        {
             string classAttribute = reader.GetAttribute(Attributes.classType);
             if (!string.IsNullOrEmpty(classAttribute)) fieldType = Type.GetType(Xmlifier.UnXmlify(classAttribute));
             Converter converter = converterLookup.GetConverter(fieldType);
@@ -64,16 +66,20 @@ namespace Xstream.Core {
                 return Unmarshal(fieldType);
         }
 
-        public void ProcessField(FieldInfo field, string serializeFieldName) {
+        public void ProcessField(FieldInfo field, string serializeFieldName)
+        {
             reader.MoveDown(serializeFieldName);
-//            field.SetValue(currentObject, ConvertField(field.FieldType));
+            //            field.SetValue(currentObject, ConvertField(field.FieldType));
             reader.MoveUp();
-            
+
         }
     }
 
-    internal class DefaultMapper : IMapper {
-        public void ProcessFieldsIn(Type type, IFieldProcessor fieldProcessor) {
+
+    internal class DefaultMapper : IMapper
+    {
+        public IEnumerable<Field> GetSerializableFieldsIn(Type type)
+        {
             FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
             foreach (var field in fields)
             {
@@ -84,10 +90,10 @@ namespace Xstream.Core {
                 Match match = Constants.AutoPropertyNamePattern.Match(field.Name);
                 if (match.Success)
                     serializeFieldName = match.Result("$1");
+                yield return new Field(field, serializeFieldName);
 
-
-                fieldProcessor.ProcessField(field, serializeFieldName);
             }
+
         }
     }
 }
