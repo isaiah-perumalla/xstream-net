188254651180595c026dedb701340bf938a77127
unknown <isaiah@.(none)>
2010-03-31 19:31:06 +0100

    code clean up

--- src/XStream.Core/XStream.cs
+++ src/XStream.Core/XStream.cs
@@ -1,16 +1,12 @@
-using System.Collections.Generic;
-using System.Reflection;
 using System.Text;
 using xstream;
-using Xstream.Core;
 using Xstream.Core.Converters;
 using Xstream.Core.Mappers;
 
 namespace Xstream.Core {
     public class XStream {
         private readonly ConverterLookup converterLookup = new ConverterLookup();
-        
-        private readonly List<Assembly> assemblies = new List<Assembly>();
+
         private IMapper mapper = new DefaultMapper();
 
         public string ToXml(object value) {
