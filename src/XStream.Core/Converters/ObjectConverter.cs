43abccee72261298025ccdfd9ad78097e51afe30
isaiahperumalla <isaiahperumalla@gmail.com>
2010-04-06 12:27:07 +0100

    working towards removing unmarshaller

--- src/XStream.Core/Converters/ObjectConverter.cs
+++ src/XStream.Core/Converters/ObjectConverter.cs
@@ -19,11 +19,49 @@ namespace Xstream.Core.Converters {
             return true;
         }
 
-        public void Marshall(object value, XStreamWriter writer, MarshallingContext context)
-        {
-            throw new NotImplementedException();
+        public void Marshall(object value, XStreamWriter writer, MarshallingContext context) {
+            Type type = value.GetType();
+            if (type.Equals(typeof(object))) return;
+            foreach (var field in mapper.GetSerializableFieldsIn(type))
+            {
+                writer.StartNode(field.SerializedName);
+                WriteClassNameIfNeedBe(value, field, writer);
+                context.ConvertAnother(field.GetObjectFrom(value));
+                writer.EndNode();
+            }
+            MarshalAs(value, type.BaseType, writer, context);
         }
 
+        private void MarshalAs(object value, Type type, XStreamWriter writer, MarshallingContext context)
+        {
+            if (type.Equals(typeof(object))) return;
+            foreach (var field in mapper.GetSerializableFieldsIn(type))
+            {
+                writer.StartNode(field.SerializedName);
+                WriteClassNameIfNeedBe(value, field, writer);
+                context.ConvertAnother(field.GetObjectFrom(value));
+                writer.EndNode();
+            }
+            Type type1 = type.BaseType;
+            if (type1.Equals(typeof(object))) return;
+            foreach (var field in mapper.GetSerializableFieldsIn(type1))
+            {
+                writer.StartNode(field.SerializedName);
+                WriteClassNameIfNeedBe(value, field, writer);
+                context.ConvertAnother(field.GetObjectFrom(value));
+                writer.EndNode();
+            }
+            MarshalAs(value, type1.BaseType, writer, context);
+        }
+
+        private void WriteClassNameIfNeedBe(object value, Field field, XStreamWriter writer)
+        {
+            object fieldValue = field.GetObjectFrom(value);
+            if (fieldValue == null) return;
+            Type actualType = fieldValue.GetType();
+            if (!field.FieldType.Equals(actualType))
+                writer.WriteAttribute(XsAttribute.classType, actualType.AssemblyQualifiedName);
+        }
         public object UnMarshall(XStreamReader reader, UnmarshallingContext context, Type type)
         {
             var result = context.FindReferenceFromCurrentNode();
